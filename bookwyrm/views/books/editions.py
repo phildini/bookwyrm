fca821a5c10e775082f5cd9439639aab8ffc9c7d
Adeodato Simó <dato@users.noreply.github.com>
2024-01-13 18:55:13 +0100

    Invalidate `active_shelf` when switching editions

--- bookwyrm/views/books/editions.py
+++ bookwyrm/views/books/editions.py
@@ -3,6 +3,7 @@ from functools import reduce
 import operator
 
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache as django_cache
 from django.core.paginator import Paginator
 from django.db import transaction
 from django.db.models import Q
@@ -104,13 +105,10 @@ def switch_edition(request):
         readthrough.book = new_edition
         readthrough.save()
 
-    reviews = models.Review.objects.filter(
-        book__parent_work=new_edition.parent_work, user=request.user
+    django_cache.delete_many(
+        [
+            f"active_shelf-{request.user.id}-{book_id}"
+            for book_id in new_edition.parent_work.editions.values_list("id", flat=True)
+        ]
     )
-    for review in reviews.all():
-        # because ratings are a subclass of reviews,
-        # this will pick up both ratings and reviews
-        review.book = new_edition
-        review.save()
-
     return redirect(f"/book/{new_edition.id}")
