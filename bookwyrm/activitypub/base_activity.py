b9bd7af154a0ab622156d47e2c04028d83aaba0b
Philip James <phildini@phildini.net>
2024-02-29 19:45:15 -0800

    Update base_activity.py

--- bookwyrm/activitypub/base_activity.py
+++ bookwyrm/activitypub/base_activity.py
@@ -424,6 +424,7 @@ def get_activitypub_data(url):
                 "Date": now,
                 "Signature": make_signature("get", sender, url, now),
             },
+            timeout=15,
         )
     except requests.RequestException:
         raise ConnectorException()
