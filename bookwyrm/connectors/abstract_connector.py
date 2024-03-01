4e196e9566c6c7d6de34e45d51c8d9660cac7415
Margaret Fero <MaggieFero@users.noreply.github.com>
2024-02-29 17:18:52 -0800

    Pylint ignore deprecation of imghdr module for now
    After discussion of the deprecation of the imghdr module, we want Pylint to ignore it for now and will return to checking out alternatives after Pycon, because we expect there will be some better options by then.

--- bookwyrm/connectors/abstract_connector.py
+++ bookwyrm/connectors/abstract_connector.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import Optional, TypedDict, Any, Callable, Union, Iterator
 from urllib.parse import quote_plus
-import imghdr
+# pylint: disable-next=deprecated-module
+import imghdr #This module was deprecated in 3.11 to be removed in 3.13, but no good alternative exists yet.
 import logging
 import re
 import asyncio
